8dcf54956cc0a00bf92e72265a9ad456b98cbf01
tomaszjdz <tomaszjdz@gmail.com>
2019-11-19 11:22:37 +0100

    init module qajunit, create .gitignore.file

--- qajunit/src/test/java/com/jsystems/qa/qajunit/JunitTest.java
+++ qajunit/src/test/java/com/jsystems/qa/qajunit/JunitTest.java
@@ -112,6 +112,48 @@ public class JunitTest extends ConfigJunit {
 
 
 
+    @Test
+
+    public void stringtest() {
+
+
+
+        String simpleString = "simpleString";
+
+
+
+        String simple = "simpleString";
+
+
+
+        String simpleString_2 = new String("simpleString");
+
+
+
+        String simpleString_3 = new String("simpleString");
+
+
+
+        assertTrue(simpleString == "simpleString");
+
+        assertTrue(simpleString == simple);
+
+        assertFalse(simpleString == simpleString_2);
+
+        assertFalse(simpleString_2 == simpleString_3);
+
+        assertTrue(simpleString.equals(simple));
+
+        assertTrue(simpleString_2.equals(simpleString_3));
+
+        int a = 1;
+
+        Integer a_1 = 1;
+
+    }
+
+
+
     @Nested
 
     @Tag("nested")
