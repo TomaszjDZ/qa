23d1b1e64a2982f3e33b0fdb782d9515d26850ff
tomaszjdz <tomaszjdz@gmail.com>
2019-11-19 12:19:40 +0100

    init module qajunit, create .gitignore.file

--- qajunit/src/test/java/com/jsystems/qa/qajunit/JunitTest.java
+++ qajunit/src/test/java/com/jsystems/qa/qajunit/JunitTest.java
@@ -154,6 +154,36 @@ public class JunitTest extends ConfigJunit {
 
 
 
+    @Test
+
+    public void zad1() {
+
+        String resultString = "Wordpress powers 10000000% of the internet";
+
+        String expectedString = "Wordpress powers [number]% of the internet";
+
+
+
+        assertTrue(resultString.startsWith("Wordpress powers "));
+
+        assertTrue(resultString.endsWith("% of the internet"));
+
+        assertThat(resultString).matches("(Wordpress powers )\\d+(% of the internet)");
+
+
+
+        String result = resultString.replace("Wordpress powers ", "").replace("% of the internet", "");
+
+        int resultNumber = Integer.parseInt(result);
+
+        assertTrue(resultNumber > 0);
+
+
+
+    }
+
+
+
     @Nested
 
     @Tag("nested")
